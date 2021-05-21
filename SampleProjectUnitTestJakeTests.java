9c7fa9127552dbc7e7172a6592ee7894366fc691
JakeL1001 <jakelyell@iinet.net.au>
2021-05-21 13:18:34 +0800

    q4 extra tests

--- SampleProjectUnitTestJakeTests.java
+++ SampleProjectUnitTestJakeTests.java
@@ -225,6 +225,11 @@ public class SampleProjectUnitTestJakeTests {
         speeds = new int[][] { { 10, 10 }, { 4, 2, 8 }, { 10 }, { 9 }, { 6, 10 }, {} };
 
         testInt(pref + "backtracking", proj.maxDownloadSpeed(adjlist, speeds, 0, 5), 19);
+
+        adjlist = new int[][] { { 1, 2 }, { 2, 3 }, { 1, 4 }, { 2, 5 }, { 3, 5 }, {} };
+        speeds = new int[][] { { 16, 13 }, { 10, 12 }, { 4, 14 }, { 9, 20 }, { 7, 4 }, {} };
+
+        testInt(pref + "online test", proj.maxDownloadSpeed(adjlist, speeds, 0, 5), 23);
     }
 
     public static void printSummary() {
